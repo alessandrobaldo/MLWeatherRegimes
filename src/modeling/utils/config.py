d4c6bed10b644b020c23d09ae104b60371fc8bf3
Alessandro Baldo <s236651@studenti.polito.it>
2022-04-03 11:16:36 +0200

    Cleaned the directory

--- src/modeling/utils/config.py
+++ src/modeling/utils/config.py
@@ -1,17 +1,8 @@
 import json
 
-with open('W:/UK/Research/Private/WEATHER/STAGE_ABALDO/scripts/src/modeling/utils/config.json', 'r') as f:
+with open('src/modeling/utils/config.json', 'r') as f:
     config = json.load(f)
 
-'''
-READ_PATH = 'P:\CH\Weather Data\ERA-5\GEOPOTENTIAL'
-physical_qty = 'Geopotential-500hPa'
-G = 9.80665
-freq = 'hourly' # 'monthly'
-months = 'DecJanFeb'#'DecJanFeb'  #'MayJunJulAugSep'
-obs_years = '1979-2020'#'1979-2020'
-LAT, LONG = (20.,80.), (-90., 30.)
-'''
 
 READ_PATH = config['readpath']
 physical_qty = config['variable']
--- src/training.py
+++ src/training.py
@@ -5,9 +5,9 @@ from modeling.utils.config import *
 from modeling.utils.plotting import *
 
 dt = build_data()
-#dt = weighted_anomaly(dt)
+dt = weighted_anomaly(dt)
 
-#pivot_anomaly = flat_table(dt)
+pivot_anomaly = flat_table(dt)
 reduction = 'VAE'
 if reduction == "PCA":
     reduced_anomaly = reduce_dim(pivot_anomaly, method='PCA', exp_variance=0.999)
